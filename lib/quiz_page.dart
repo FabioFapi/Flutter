2fe0d96f33b2a2f20386fb3bcd3e4d25a11cbd29
FabioFapi <fabio.recchia@edu.itspiemonte.it>
2024-10-03 12:16:03 +0200

    03/10/2024 12:15

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -1,104 +1,124 @@
 import 'package:flutter/material.dart';
-import 'package:my_app_flutter/colors.dart';
+import 'colors.dart';
 
-class QuizPage extends StatelessWidget {
-  const QuizPage({Key? key}) : super(key: key);
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  String? selectedAnswer;
+  final answers = ["Big Hero 6", "Shrek", "Wall-E", "Gabibbo"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: quizBackgroundColor,
-      body: Column(
-        children: [
-          SizedBox(height: 50,),
-          Image.network("https://upload.wikimedia.org/wikipedia/it/7/77/Shrek_%28film%29.png"),
-          const Text("What is the name of the Movies?",
-            style: TextStyle(
-                color: questionTextColor,
-                fontSize: 20,
-                fontWeight: FontWeight.bold
-            ),
-          ),
-          const Text("PLease choose one of the options "
-              "below and confirm your answer by clicking "
-              "the Confirm button",
-            style: TextStyle(
-                color: hintTextColor,
-                fontSize: 14,
-                fontWeight: FontWeight.bold
-            ),
-          ),
-          AnswerButton("Big Hero 6"),
-          AnswerButton("Shrek"),
-          AnswerButton("Wall-E"),
-          AnswerButton("Gabibbo"),
-          const Spacer(flex: 1,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              FilledButton(onPressed: () {},
-                  child: const Text("CONFERMACAZZO",
+        backgroundColor: quizBackgroundColor,
+        body: Column(
+          children: [
+            Image.network(
+                "https://upload.wikimedia.org/wikipedia/it/7/77/Shrek_%28film%29.png"),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+              child: Column(
+                children: [
+                  const Text(
+                    "What is the name of the Movies?",
+                    style: TextStyle(
+                        color: questionTextColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      "PLease choose one of the options "
+                          "below and confirm your answer by clicking "
+                          "the Confirm button",
                       style: TextStyle(
-                        color: orange ,
+                          color: hintTextColor,
                           fontSize: 14,
-                          fontWeight: FontWeight.bold
-                      ))
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  ...answers.map((answer) => AnswerButton(
+                    answer,
+                    onPressed: () {
+                      setState(() {
+                        selectedAnswer = answer;
+                      });
+                    },
+                    isSelected: selectedAnswer == answer,
+                  )),
+                ],
               ),
-            ],
-          ),
-        ],
-      ),
-    );
+            ),
+            const Spacer(flex: 1),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FilledButton(
+                    onPressed: () {},
+                    style: const ButtonStyle(
+                        backgroundColor:
+                        WidgetStatePropertyAll(questionTextColor)),
+                    child: const Text(
+                      "Confirm",
+                      style: TextStyle(
+                          color: confirmButton,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
+                    )),
+              ],
+            ),
+          ],
+        ));
   }
 }
 
 class AnswerButton extends StatelessWidget {
-
   final String text;
-
   final bool isSelected;
+  final Function() onPressed;
 
-  const AnswerButton(this.text,{this.isSelected = false ,super.key});
-
+  const AnswerButton(this.text,
+      {required this.onPressed, this.isSelected = false, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 8),
       child: InkWell(
-        onTap: () {
-        },
+        onTap: onPressed,
         borderRadius: BorderRadius.circular(10),
         child: Container(
-          width: double.infinity,
-          height: 44,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: buttonColor,
-            border: Border.all(
-              color: isSelected ? answerButtonCheckColor : Colors.transparent,
-              width: 2
-            )
-            ),
-          child: Padding(
-            padding: const EdgeInsets.only(right: 8, left: 16),
-          child: Row (
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [ Text(text,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 14,
-            ),
-          ),
-            Opacity(
-              opacity: isSelected ? 1.0 : 0.2,
-              child : const Icon(Icons.check, color: answerButtonCheckColor,),
-            ),
-          ],
-        ),
-      ),
-    ),
+            width: double.infinity,
+            height: 44,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: buttonColor,
+                border: Border.all(
+                    color: isSelected
+                        ? answerButtonCheckColor
+                        : Colors.transparent,
+                    width: 2)),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16, right: 8),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(text,
+                      style:
+                      const TextStyle(color: Colors.white, fontSize: 14)),
+                  Opacity(
+                      opacity: isSelected ? 1.0 : 0.2,
+                      child: const Icon(Icons.check,
+                          color: answerButtonCheckColor)),
+                ],
+              ),
+            )),
       ),
     );
   }
