925d7cdf946087eaea89696e0965cd4f09644d78
Fabio Recchia <fabio.recchia@edu.itspiemonte.it>
2024-10-03 09:55:02 +0200

    PrimoCommit

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const darkPurple = Color(0xFF361757);
+const lightPurple = Color(0xFF4F257B);
+const orange = Color(0xFFFDB94B);
+
+
+
+const buttonColor = darkPurple;
+const quizBackgroundColor = lightPurple;
+const questionTextColor = orange;
+const hintTextColor = Colors.white;
+const confirmButton = Colors.black;
