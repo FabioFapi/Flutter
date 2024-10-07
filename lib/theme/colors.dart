e74370d96d0b19d6133c8ff3fc1dea094164f346
Fabio Recchia <fabio.recchia@edu.itspiemonte.it>
2024-10-07 16:57:13 +0200

    07/10/2024 16:57

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
+
 const darkPurple = Color(0xFF361757);
 const lightPurple = Color(0xFF4F257B);
 const orange = Color(0xFFFDB94B);
 
-
-
 const buttonColor = darkPurple;
-const quizBackgroundColor = lightPurple;
+const backgroundColor = lightPurple;
 const questionTextColor = orange;
 const hintTextColor = Colors.white;
-const confirmButton = Colors.black;
+const confirmButtonColor = orange;
+const confirmButtonTextColor = darkPurple;
+const answerButtonCheckColor = orange;
